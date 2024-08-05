459231873f9277d0ecdfc4098a189a73778ab0f9
Isaac Nicholls <isaac.nicholls@kraken.tech>
2024-08-06 01:43:51 +1200

    Adding circuit name

--- api.py
+++ api.py
@@ -35,8 +35,10 @@ session_data = json.loads(response.read().decode('utf-8'))
 session_keys = [item["session_key"] for item in session_data]
 country_code = [item["country_code"] for item in session_data]
 session_type = [item["session_type"] for item in session_data]
-
-combined_data_session = list(zip(session_keys, country_code,session_type))
+circuit_name = [item["circuit_short_name"] for item in session_data]
+circuit_key = [item["circuit_key"] for item in session_data]
+print(session_url)
+combined_data_session = list(zip(session_keys, country_code,session_type,circuit_name,circuit_key))
 
 print(combined_data_session)
 
