191decf2f071992ab220779dcbc8aa849ab16790
Isaac Nicholls <isaac.nicholls@kraken.tech>
2024-08-06 01:19:06 +1200

    Fixes to allow position and driver data to run TODO: Support multiple sessions

--- api.py
+++ api.py
@@ -26,9 +26,11 @@ variables = [
     'country_name='
 ]
 year = '2023'
+and_session = '&'+variables[2]+'9197'
+session = variables[2]+'9197'
 
 
-session_url = base + methods[0] + variables[0] + year
+session_url = base + methods[0] + variables[0] + year+and_session
 response = urlopen(session_url)
 session_data = json.loads(response.read().decode('utf-8'))
 session_keys = [item["session_key"] for item in session_data]
@@ -39,7 +41,7 @@ combined_data_session = list(zip(session_keys, country_code,session_type))
 
 print(combined_data_session)
 
-driver_url = base + methods[2]
+driver_url = base + methods[2]+session
 response = urlopen(driver_url)
 driver_data = json.loads(response.read().decode('utf-8'))
 broadcast_name = [item["broadcast_name"] for item in driver_data]
@@ -47,17 +49,15 @@ driver_number = [item["driver_number"] for item in driver_data]
 session_key_driver = [item["session_key"] for item in driver_data]
 team_name = [item["team_name"] for item in driver_data]
 
-combined_data_driver = list(zip(broadcast_name,driver_number,session_key_driver,team_name))
+combined_data_driver = list(zip(broFixes adcast_name,driver_number,session_key_driver,team_name))
 
 print(combined_data_driver)
 
 #Position data
 
-for key in session_keys:
-     position_url = base + methods[8] + variables[2] + str(key)
+position_url = base + methods[8]+session
 response = urlopen(position_url)
 position_data = json.loads(response.read().decode('utf-8'))
-
 driver_number_position = [item["driver_number"] for item in position_data]
 session_key_position = [item["session_key"] for item in position_data]
 position = [item["position"] for item in position_data]
