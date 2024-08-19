d51fdf0211120479ba951d1afd55272b5bc389a5
Isaac Nicholls <isaac.nicholls@kraken.tech>
2024-08-20 02:53:33 +1200

    Adding TODO

--- elements.py
+++ elements.py
@@ -58,7 +58,7 @@ def session_input(button, layout, line_edit):  # todo: only allow 1 press #TODO:
     response = urlopen(session_url)
     session_data = json.loads(response.read().decode('utf-8'))
     circuit_name = [item["circuit_short_name"] for item in session_data]
-
+#todo if empty list display error
     for sessions in circuit_name:
         layout.addWidget(QLabel(sessions))
 
