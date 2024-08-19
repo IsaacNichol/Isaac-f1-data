045e27fd4c7b0686c96c1dfb531c6a8bafebf2ed
Isaac Nicholls <isaac.nicholls@kraken.tech>
2024-08-20 00:21:06 +1200

    Added lables and buttons to the layout

--- PyQt.py
+++ PyQt.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton,QHBoxLayout
 import sys
 
 app = QApplication(sys.argv)
@@ -9,6 +9,23 @@ window.setWindowTitle("F1 Data Downloader")
 
 # Create layout
 layout = QVBoxLayout()
+
+# Create label for window
+
+text_header = ["Bootstrap the database?",
+        "This creates all the needed schema on a pre-existing postgres database."]
+for header in text_header:
+    layout.addWidget(QLabel(header))
+
+# Create a horizontal layout for the buttons
+button_layout = QHBoxLayout()
+
+# Add "Yes" and "No" buttons to the horizontal layout
+button_layout.addWidget(QPushButton('Yes'))
+button_layout.addWidget(QPushButton('No'))
+
+# Add the horizontal layout with buttons to the main layout
+layout.addLayout(button_layout)
 
 # Create the window
 window.setLayout(layout)
